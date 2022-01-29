96d980139ff3fb3dcc63b183f63f51287fb188ee
dwcalade <dwcalade@yahoo.ca>
2022-01-28 16:46:44 -0800

    commit on main

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "dwcalade@yahoo.ca";
+$subject = "Mail From Portfolio Website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@dwcalade.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
